fd1a1daa408831398c165c8df63f5647b44879b3
k-gn <rlarbska97@gmail.com>
2023-04-23 19:00:11 +0900

    chore: application 영역에서 repository 구현체 제거

--- core/src/main/java/kr/co/toppings/core/infrastructure/persistence/user/UserRepositoryImpl.java
+++ core/src/main/java/kr/co/toppings/core/infrastructure/persistence/user/UserRepositoryImpl.java
@@ -1,6 +1,13 @@
 package kr.co.toppings.core.infrastructure.persistence.user;
 
-import kr.co.toppings.core.application.user.persistence.UserRepository;
+import org.springframework.stereotype.Repository;
 
+import kr.co.toppings.core.application.user.persistence.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
 public class UserRepositoryImpl implements UserRepository {
+
+	private final UserJpaRepository userJpaRepository;
 }
