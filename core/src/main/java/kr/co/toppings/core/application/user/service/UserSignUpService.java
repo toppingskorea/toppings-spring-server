577be5ce4e08af98b42a20339b73e15924e4fbb4
Haebin <hbyeon@student.42seoul.kr>
2023-04-28 10:51:32 +0900

    chore: Rename Service Logic

--- core/src/main/java/kr/co/toppings/core/application/user/service/UserSignUpService.java
+++ core/src/main/java/kr/co/toppings/core/application/user/service/UserSignUpService.java
@@ -10,7 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class UserLoginService {
+public class UserSignUpService {
 
     private final UserRepository userRepository;
 
--- core/src/test/java/kr/co/toppings/core/user/service/UserSignUpTest.java
+++ core/src/test/java/kr/co/toppings/core/user/service/UserSignUpTest.java
@@ -2,16 +2,10 @@ package kr.co.toppings.core.user.service;
 
 import kr.co.toppings.core.application.user.dto.request.UserProfile;
 import kr.co.toppings.core.application.user.persistence.UserRepository;
-import kr.co.toppings.core.application.user.service.UserLoginService;
+import kr.co.toppings.core.application.user.service.UserSignUpService;
 import kr.co.toppings.core.domain.user.User;
-import kr.co.toppings.core.domain.user.constants.Country;
-import kr.co.toppings.core.fixture.UserFixture;
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -24,22 +18,22 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 @Slf4j
 @SpringBootTest
-@DisplayName("로그인 서비스 테스트")
-public class UserServiceTest {
+@DisplayName("회원가입 서비스 테스트")
+public class UserSignUpTest {
     @Autowired
     private UserRepository userRepository;
 
     @Autowired
-    private UserLoginService userLoginService;
+    private UserSignUpService userSignUpService;
 
     @Test
-    @DisplayName("로그인 ")
+    @DisplayName("[SignUp]")
     void signUpSuccess() throws Exception {
         //given
         UserProfile userProfile = new UserProfile(A.getName(), A.getEmail(), A.getCountry());
 
         //when
-        Long savedUserId = userLoginService.signUpUser(userProfile);
+        Long savedUserId = userSignUpService.signUpUser(userProfile);
 
         //then
         Optional<User> result = userRepository.findById(savedUserId);
