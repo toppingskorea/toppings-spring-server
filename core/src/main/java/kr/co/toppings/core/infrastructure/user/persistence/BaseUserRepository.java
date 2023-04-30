236b0752fecc459f7fcef9e05d612edae90ec215
Haebin <hbyeon@student.42seoul.kr>
2023-05-01 02:30:43 +0900

    feat: Compile Error fix

--- core/src/main/java/kr/co/toppings/core/infrastructure/user/persistence/BaseUserRepository.java
+++ core/src/main/java/kr/co/toppings/core/infrastructure/user/persistence/BaseUserRepository.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 
 @Repository
 @RequiredArgsConstructor
-public class BaseUserRepository implements UserRepository {
+public class BaseUserRepository {
 
 	private final UserJpaRepository userJpaRepository;
 }
