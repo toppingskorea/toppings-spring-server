3ca1843917d591c6fc5e80051fdc6303fefc2ae1
Haebin <hbyeon@student.42seoul.kr>
2023-05-07 23:43:39 +0900

    feat: Edit ErrorCode Form / Add User Nickname Exception

--- core/src/main/java/kr/co/toppings/core/global/error/ErrorCode.java
+++ core/src/main/java/kr/co/toppings/core/global/error/ErrorCode.java
@@ -1,5 +1,9 @@
 package kr.co.toppings.core.global.error;
 
+import static org.springframework.http.HttpStatus.*;
+
+import org.springframework.http.HttpStatus;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -8,12 +12,15 @@ import lombok.RequiredArgsConstructor;
 public enum ErrorCode {
 
 	// Common
-	INVALID_INPUT_VALUE("C001", "Invalid Input Value", 400),
-	INTERNAL_SERVER_ERROR("C002", "Server Error", 500),
-	METHOD_NOT_ALLOWED("C002", "지원하지 않은 HTTP Method 입니다.", 405)
-	;
+	INVALID_INPUT_VALUE("C001", "Invalid Input Value", HttpStatus.BAD_REQUEST),
+	INTERNAL_SERVER_ERROR("C002", "Server Error", HttpStatus.INTERNAL_SERVER_ERROR),
+	METHOD_NOT_ALLOWED("C003", "지원하지 않은 HTTP Method 입니다.", HttpStatus.METHOD_NOT_ALLOWED),
+
+	// User
+	USER_INVALID_NICKNAME("U001", "닉네임 형식이 맞지 않습니다.", HttpStatus.BAD_REQUEST),
+	USER_DUPLICATED_NICKNAME("U002", "이미 존재하는 닉네임 입니다.", HttpStatus.CONFLICT);
 
 	private final String code;
 	private final String message;
-	private final int status;
+	private final HttpStatus status;
 }
