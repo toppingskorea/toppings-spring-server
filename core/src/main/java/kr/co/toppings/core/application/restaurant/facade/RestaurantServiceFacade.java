a5898ed95b5c77069b7542f0bc2d164137058416
k-gn <rlarbska97@gmail.com>
2023-05-07 17:54:30 +0900

    feat: add service annotation

--- core/src/main/java/kr/co/toppings/core/application/restaurant/facade/RestaurantServiceFacade.java
+++ core/src/main/java/kr/co/toppings/core/application/restaurant/facade/RestaurantServiceFacade.java
@@ -1,4 +1,11 @@
 package kr.co.toppings.core.application.restaurant.facade;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class RestaurantServiceFacade {
+
+
 }
--- core/src/main/java/kr/co/toppings/core/application/restaurant/service/RestaurantFindService.java
+++ core/src/main/java/kr/co/toppings/core/application/restaurant/service/RestaurantFindService.java
@@ -1,4 +1,14 @@
 package kr.co.toppings.core.application.restaurant.service;
 
+import kr.co.toppings.core.application.restaurant.persistence.RestaurantQueryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class RestaurantFindService {
+
+    private final RestaurantQueryRepository restaurantQueryRepository;
 }
