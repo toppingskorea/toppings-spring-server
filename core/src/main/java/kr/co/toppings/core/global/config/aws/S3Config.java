d657cbea8bd11934df29662d5715094bb4689fb3
k-gn <rlarbska97@gmail.com>
2023-05-12 00:50:02 +0900

    chore: test ci

--- core/src/main/java/kr/co/toppings/core/global/config/aws/S3Config.java
+++ core/src/main/java/kr/co/toppings/core/global/config/aws/S3Config.java
@@ -10,8 +10,10 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("dev")
 public class S3Config {
 
 	@Value("${cloud.aws.credentials.accessKey}")
