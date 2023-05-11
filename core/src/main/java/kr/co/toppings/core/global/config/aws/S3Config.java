749ba7274ddc217f9732ab85bb4a2ac12e97a524
k-gn <rlarbska97@gmail.com>
2023-05-12 02:10:17 +0900

    chore: test ci

--- core/src/main/java/kr/co/toppings/core/global/config/aws/S3Config.java
+++ core/src/main/java/kr/co/toppings/core/global/config/aws/S3Config.java
@@ -13,10 +13,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class S3Config {
 
-	@Value("${cloud.aws.credentials.accessKey}")
+	@Value("${cloud.aws.credentials.access-key}")
 	private String accessKey;
 
-	@Value("${cloud.aws.credentials.secretKey}")
+	@Value("${cloud.aws.credentials.secret-key}")
 	private String secretKey;
 
 	@Bean
