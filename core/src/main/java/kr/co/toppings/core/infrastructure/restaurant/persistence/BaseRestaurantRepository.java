a031623bcf2d426d1ab6489da4af305598054973
k-gn <rlarbska97@gmail.com>
2023-05-19 23:27:21 +0900

    feat: add transaction annotation

--- core/src/main/java/kr/co/toppings/core/infrastructure/restaurant/persistence/BaseRestaurantRepository.java
+++ core/src/main/java/kr/co/toppings/core/infrastructure/restaurant/persistence/BaseRestaurantRepository.java
@@ -4,9 +4,11 @@ import kr.co.toppings.core.application.restaurant.persistence.RestaurantReposito
 import org.springframework.stereotype.Repository;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 @RequiredArgsConstructor
+@Transactional
 public class BaseRestaurantRepository implements RestaurantRepository {
 
 	private final RestaurantJpaRepository restaurantJpaRepository;
